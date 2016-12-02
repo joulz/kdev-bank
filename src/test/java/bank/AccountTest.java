dfc72ad231a4671d00c9fd6974facbce1db5760c
Julian <julian.lemke@siemens.com>
2016-12-02 11:18:09 +0100

    close account

--- src/test/java/bank/AccountTest.java
+++ src/test/java/bank/AccountTest.java
@@ -1,8 +1,6 @@
 package bank;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Rule;
--- src/test/java/bank/BankTest.java
+++ src/test/java/bank/BankTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertThat;
 
 import java.util.NoSuchElementException;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -16,9 +17,10 @@ public class BankTest {
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
+	private Bank bank;
+
 	@Test
 	public void opened_account_should_be_stored_in_the_bank() {
-		Bank bank = new Bank();
 		AccountId accountId = bank.openAccount("Customer Name");
 		Account account = bank.getAccount(accountId);
 		assertThat(account, is(not(nullValue())));
@@ -28,7 +30,6 @@ public class BankTest {
 
 	@Test
 	public void multiple_opened_accounts_should_have_different_ids() {
-		Bank bank = new Bank();
 		AccountId accountId1 = bank.openAccount("Customer One");
 		AccountId accountId2 = bank.openAccount("Customer Two");
 		assertThat(accountId1, is(not(equalTo(accountId2))));
@@ -36,16 +37,36 @@ public class BankTest {
 
 	@Test
 	public void customer_must_have_a_not_empty_name() {
-		Bank bank = new Bank();
 		expectedException.expect(IllegalArgumentException.class);
 		bank.openAccount("");
 	}
-	
+
 	@Test
 	public void accessing_account_by_unkownId_should_fail() {
-		Bank bank = new Bank();
 		AccountId accountId = new AccountId(123);
 		expectedException.expect(NoSuchElementException.class);
 		bank.getAccount(accountId);
+	}
+
+	@Before
+	public void setup() {
+		bank = new Bank();
+	}
+
+	@Test
+	public void a_closed_account_can_not_be_found_anymore() throws Exception {
+		AccountId accountId = bank.openAccount("customer");
+		bank.closeAccount(accountId);
+		expectedException.expect(NoSuchElementException.class);
+		bank.getAccount(accountId);
+	}
+
+	@Test
+	public void account_can_only_by_closed_with_zero_balance() throws Exception {
+		AccountId accountId = bank.openAccount("customer");
+		Account account = bank.getAccount(accountId);
+		account.deposit(1000l);
+		expectedException.expect(IllegalStateException.class);
+		bank.closeAccount(accountId);
 	}
 }
